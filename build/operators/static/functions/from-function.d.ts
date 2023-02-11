8e6583af4d314f0828dde250a0a788625cf7595e
thomas.toledo <thomas.toledo@eemi.com>
2023-02-12 00:59:11 +0100

    move ProxyObservable back to from-function file to avoid cycling dependencies

--- build/operators/static/functions/from-function.d.ts
+++ build/operators/static/functions/from-function.d.ts
@@ -1,7 +1,13 @@
-import { Types } from "../../../models/types";
-import { ProxyObservable } from "../../../observable/proxy-observable";
+import { Types } from "../../../models";
+import { Observable } from "../../../observable/observable";
 export interface ProxyObservableFunction<T = never> extends Types.Observable<T> {
     (...args: any): any;
     asObservable: () => ProxyObservable<T>;
 }
+declare class ProxyObservable<T = never> extends Observable<T> implements Types.Observable<T> {
+    private proxy;
+    private constructor();
+    static create<T>(f: (...args: any) => T): ProxyObservableFunction<T>;
+}
 export declare const fromFunction: <T = any>(f: (...args: any) => T) => ProxyObservableFunction<T>;
+export {};
--- src/operators/static/functions/from-function.ts
+++ src/operators/static/functions/from-function.ts
@@ -1,14 +1,54 @@
-import { Types } from "../../../models/types";
-import { ProxyObservable } from "../../../observable/proxy-observable";
+import { OnNext, OperationResult, OperationResultFlag, Subscriber, Subscription, Types } from "../../../models";
+import { Observable } from "../../../observable/observable";
 
-export interface ProxyObservableFunction<T = never>
-  extends Types.Observable<T> {
+export interface ProxyObservableFunction<T = never> extends Types.Observable<T> {
   (...args: any): any;
   asObservable: () => ProxyObservable<T>;
 }
 
-export const fromFunction = function <T = any>(
-  f: (...args: any) => T
-): ProxyObservableFunction<T> {
-  return ProxyObservable.create(f);
-};
+class ProxyObservable<T = never> extends Observable<T> implements Types.Observable<T> {
+  private proxy: {(...args: any): T};
+
+  private constructor(f: (...args: any) => T) {
+      super();
+      this.proxy = Object.assign(new Proxy(f, {
+          apply: (target, thisArg, args) => {
+              let res;
+              let operationResult: OperationResult<T>;
+              try {
+                  operationResult = new OperationResult(res = target.apply(thisArg, args));
+              } catch(e) {
+                  operationResult = new OperationResult(res as any, OperationResultFlag.OperationError, e as Error);
+              }
+
+              this.innerSequence = [operationResult];
+              this._subscribers.forEach((s) => this.executeSubscriber(this.innerSequence, s));
+
+              if (operationResult.isOperationError()) {
+                  throw operationResult.error;
+              }
+              
+              return res;
+          },
+      }));
+  }
+
+  public static create<T>(f: (...args: any) => T): ProxyObservableFunction<T> {
+      const instance = new ProxyObservable<T>(f);
+      const res: ProxyObservableFunction<T> = (...args: any[]) => instance.proxy(...args);
+
+      instance.innerSequence = [];
+      res.subscribe = function(subscriber?: OnNext<T> | Subscriber<T> | undefined): Subscription {
+        return instance.subscribe.apply(instance, [subscriber ?? void 0]);
+      }
+      res.pipe = instance.pipe.bind(instance);
+      res.asObservable = () => instance;
+      return res;
+  }
+}
+  
+  export const fromFunction = function <T = any>(
+    f: (...args: any) => T
+  ): ProxyObservableFunction<T> {
+    return ProxyObservable.create(f);
+  };
